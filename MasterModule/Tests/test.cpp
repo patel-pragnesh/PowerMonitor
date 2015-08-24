1314e8cc57585c3b386bbd614efd2cf52bbfcca4
Alexis Mestag <alexis.mestag@epitech.eu>
2015-08-25 00:18:58 +0200

    Better ODB mapping for GlobalReading view

--- MasterModule/Tests/test.cpp
+++ MasterModule/Tests/test.cpp
@@ -5,7 +5,7 @@
 // Login   <mestag_a@epitech.net>
 // 
 // Started on  Mon Feb 23 22:00:09 2015 alexis mestag
-// Last update Tue Aug 25 00:02:56 2015 alexis mestag
+// Last update Tue Aug 25 00:12:47 2015 alexis mestag
 //
 
 #include		<iostream>
@@ -18,7 +18,8 @@
 int			main() {
   Database		db("powermonitor", "powermonitor", "powermonitor");
   Database::transaction_type	*t = db.getTransaction();
-  Repository<GlobalReading>::query_type	q(Repository<GlobalReading>::query_type::tp.point == 1436299435);
+  Repository<GlobalReading>::query_type	q(Repository<GlobalReading>::query_type::info.unit == 2 &&
+					  Repository<GlobalReading>::query_type::tp.point == 1436299435);
   GlobalReading			result = db.query_value(q);
 
   std::cout << result.tp.getTimeSinceEpoch() << ": " << result.value << std::endl;
